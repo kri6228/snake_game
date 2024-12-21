26f8b668f9d8926169936c05941e6bac9faa91c0
kri6228 <156495940+kri6228@users.noreply.github.com>
2024-12-21 12:23:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // game constants & variables
 let inputDir = {x: 0, y: 0};
-const foodSound = new Audio('food.mp3');
-const gameOverSound = new Audio('https://kri6228.github.io/game over.mp3');
-const movesound = new Audio('https://kri6228.github.io/move.mp3');
-const musicsound = new Audio('https://kri6228.github.io/ForestWalk.mp3');
+const foodSound = new Audio('https://kri6228.github.io/snake_game/food.mp3');
+const gameOverSound = new Audio('https://kri6228.github.io/snake_game/game over.mp3');
+const movesound = new Audio('https://kri6228.github.io/snake_game/move.mp3');
+const musicsound = new Audio('https://kri6228.github.io/snake_game/ForestWalk.mp3');
 let speed = 5;
 let score = 0;
 let lastpaintTime = 0;
